d24528d3b3cf2c9fe3953fae1324044c3798911c
Sindre Sorhus <sindresorhus@gmail.com>
2019-04-11 13:26:36 +0700

    Meta tweaks

--- test.js
+++ test.js
@@ -4,26 +4,24 @@ import inRange from 'in-range';
 import stableFn from 'stable-fn';
 import randomFloat from '.';
 
-function inRangeCheck(min, max) {
-	return stableFn(() => inRange(randomFloat(min, max), min, max));
-}
+const inRangeCheck = ({start, end}) => stableFn(() => inRange(randomFloat(start, end), {start, end}));
 
 test('main', t => {
 	t.true(numberIsFloat(randomFloat(0, 10)));
 	t.false(stableFn(() => randomFloat(1)));
-	t.true(stableFn(() => inRange(randomFloat(1), 0, 1)));
-	t.true(inRange(randomFloat(1), 0, 1));
-	t.true(inRangeCheck(0, 0.1));
-	t.true(inRangeCheck(0, 1));
-	t.true(inRangeCheck(0, 2));
-	t.true(inRangeCheck(0, 10));
-	t.true(inRangeCheck(0, 100));
-	t.true(inRangeCheck(0, 0));
-	t.true(inRangeCheck(0, 1));
-	t.true(inRangeCheck(0, 2));
-	t.true(inRangeCheck(0, 10));
-	t.true(inRangeCheck(0, 100));
-	t.true(inRangeCheck(5, 10));
-	t.true(inRangeCheck(524234213, 99999999999));
-	t.true(inRangeCheck(-10, -5));
+	t.true(stableFn(() => inRange(randomFloat(1), {start: 0, end: 1})));
+	t.true(inRange(randomFloat(1), {start: 0, end: 1}));
+	t.true(inRangeCheck({start: 0, end: 0.1}));
+	t.true(inRangeCheck({start: 0, end: 1}));
+	t.true(inRangeCheck({start: 0, end: 2}));
+	t.true(inRangeCheck({start: 0, end: 10}));
+	t.true(inRangeCheck({start: 0, end: 100}));
+	t.true(inRangeCheck({start: 0, end: 0}));
+	t.true(inRangeCheck({start: 0, end: 1}));
+	t.true(inRangeCheck({start: 0, end: 2}));
+	t.true(inRangeCheck({start: 0, end: 10}));
+	t.true(inRangeCheck({start: 0, end: 100}));
+	t.true(inRangeCheck({start: 5, end: 10}));
+	t.true(inRangeCheck({start: 524234213, end: 99999999999}));
+	t.true(inRangeCheck({start: -10, end: -5}));
 });
